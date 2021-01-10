f99008c3b456c871c37117b9d6952a39f188bc0f
jacqueline-lam <jacqueline.karin.lam@gmail.com>
2021-01-10 14:04:15 -0500

    add default movie poster placeholder

--- src/components/movies/MovieCard.js
+++ src/components/movies/MovieCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import moviePosterPlaceholder from '../../images/movie-placeholder.png';
 
 const MovieCard = ({ movie }) => {
   const title = movie.Title
   const year = movie.Year
-  const imgUrl = movie.Poster
+  const poster = movie.Poster !== "N/A" ? movie.Poster : moviePosterPlaceholder;
 
   return (
-    <div id='movie-card'>
-      <img className='movie-thumbnail img-fluid' alt='movie thumbnail' src={imgUrl} />
+    <div className='movie-card'>
+      <img className='movie-thumbnail img-fluid' alt='movie thumbnail' src={poster} />
       <h1>{title}</h1>
       <i>{year}</i>
     </div >
